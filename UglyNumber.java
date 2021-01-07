442723d1d7240929ffe4a75d98a320a2fb1ac8af
Yasir_H <45829991+Yasir77788@users.noreply.github.com>
2021-01-07 04:01:30 -0600

    Add files via upload

---
+++ UglyNumber.java
@@ -0,0 +1,43 @@
+// An ugly number is a number whose only prime factors are 2, 3 or 5
+
+public class UglyNumber
+{
+	public static void main(String[] args)
+	{
+		for(int i = 1; i < 100; ++i)
+			System.out.println(i + " is ugly number? " + isUglyNum(i));
+		
+	}
+	
+	public static boolean isUglyNum(int n)
+	{
+		if (n == 1) 
+			return true;
+		
+		while(n%2 == 0)
+		{
+			n = n/2;
+		}
+		if (n == 1)
+			return true;
+		
+		while(n%3 == 0)
+		{
+			n = n/3;
+		}
+		if (n == 1)
+			return true;
+		
+		while(n%5 == 0)
+		{
+			n = n/3;
+		}
+		if (n == 1)
+			return true;
+		else
+			return false;
+		
+		
+		
+	}
+}
